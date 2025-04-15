c4ab8ba4b4a758fe633670f3ac231d541d3882dc
annthomas <annsarahthomas@gmail.com>
2025-04-14 22:14:10 -0400

    Updated project links and name info

--- src/app/me/page.jsx
+++ src/app/me/page.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 const Page = () => {
     return (
         <div className='mx-auto p-40 border text-center'>
-            <h1 className='text-xl'>Name : Ann Thomas</h1>
-            <h1 className='text-xl'> Link to GitHub repository to React.js project Link: <Link href="https://localhost:12/34<">https://localhost:12/34</Link> </h1>
+  <h1 className='text-xl'>Name: Ann Thomas</h1>
 
-            <h1 className='text-xl'>Link to GitHub repository to Node.js project Link: <Link href="https://localhost:12/34<">https://localhost:12/34</Link> </h1>
+  <h1 className='text-xl'>
+    Link to GitHub repository for React.js project:<br />
+    https://github.com/annsthomas/onlinebookstore.git
+  </h1>
 
-        </div>
+  <h1 className='text-xl'>
+    Link to GitHub repository for Node.js project:<br />
+    https://github.com/annsthomas/onlinebookstore.git
+  </h1>
+</div>
     );
 }
 
